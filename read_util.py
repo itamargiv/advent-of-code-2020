a279adbc5ee04f7a0bdc794f3cd1891c6a2a3d7d
Itamar Givon <itamar.givon.dev@gmail.com>
2020-12-06 17:43:28 +0100

    Fix execution on import

--- read_util.py
+++ read_util.py
@@ -9,8 +9,9 @@ def readlines_batch(read_file, sep = '\n'):
 
     yield batch
 
-with open('./inputs/test-0F.txt') as given:
-    
-    assert next(readlines_batch(given)) == 'abc\n', "Cannot read single line"
-    assert next(readlines_batch(given)) == 'a\nb\nc\n', "Cannot collect multiple lines"
-    assert [batch for batch in readlines_batch(given)] == ['ab\nac\n', 'a\na\na\na\n', 'b'], "Cannot be comprehended into a list"
+
+def test_readlines_batch():
+    with open('./inputs/test-0F.txt') as given:
+        assert next(readlines_batch(given)) == 'abc\n', "Cannot read single line"
+        assert next(readlines_batch(given)) == 'a\nb\nc\n', "Cannot collect multiple lines"
+        assert [batch for batch in readlines_batch(given)] == ['ab\nac\n', 'a\na\na\na\n', 'b'], "Cannot be comprehended into a list"
