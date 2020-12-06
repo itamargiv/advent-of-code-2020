c47d5d57a40c84937b7c039ac97264fd128aa8ae
Itamar Givon <itamar.givon.dev@gmail.com>
2020-12-06 17:34:03 +0100

    Fix comprehension issues

--- read_util.py
+++ read_util.py
@@ -1,11 +1,11 @@
 def readlines_batch(read_file, sep = '\n'):
     batch = ''
-    
     for line in read_file:
         if line is sep:
             yield batch
-        
-        batch += line
+            batch = ''
+        else:
+            batch += line
 
     yield batch
 
@@ -13,3 +13,4 @@ with open('./inputs/test-0F.txt') as given:
     
     assert next(readlines_batch(given)) == 'abc\n', "Cannot read single line"
     assert next(readlines_batch(given)) == 'a\nb\nc\n', "Cannot collect multiple lines"
+    assert [batch for batch in readlines_batch(given)] == ['ab\nac\n', 'a\na\na\na\n', 'b'], "Cannot be comprehended into a list"
