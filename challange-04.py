24356e96e61d7e6c4377666e14580d00775dd9da
Itamar Givon <itamar.givon.dev@gmail.com>
2020-12-06 23:09:40 +0100

    Day 3: Introduce enumeration and sanitzation

--- challange-04.py
+++ challange-04.py
@@ -1,18 +1,11 @@
 from functools import reduce
+from read_util import strip_lines
 
-def count_octos(data, line):
-    line = line.strip()
-    position = data["position"]
+def count_octos(octo_sum, item):
+    index, line = item
+    position = index * 3
     cursor = position % len(line)
-    octo_sum = data["sum"] + 1 if line[cursor] is '#' else data["sum"]
-
-    return {
-        "sum": octo_sum,
-        "position": position + 3
-    }
+    return octo_sum + 1 if line[cursor] == '#' else octo_sum
 
 with open('./inputs/input-0C.txt') as f:
-    print(reduce(count_octos, f.readlines(), {
-        "sum": 0,
-        "position": 0
-    }))
+    print(reduce(count_octos, enumerate(strip_lines(f)), 0))
--- challange-05.py
+++ challange-05.py
@@ -1,6 +1,8 @@
 from functools import reduce
+from math import prod
+from read_util import strip_lines
 
-def create_counter(line, index):
+def create_counter(index, line):
     def count_octos(slope, octo_sum):
         if index % slope[1] != 0:
             return octo_sum
@@ -8,25 +10,17 @@ def create_counter(line, index):
         position = int(index * slope[0] / slope[1])
         cursor = position % len(line)
 
-        return octo_sum + 1 if line[cursor] is '#' else octo_sum
+        return octo_sum + 1 if line[cursor] == '#' else octo_sum
     
     return count_octos
 
 def create_sum(slopes):
-    def sum_octos(data, line):
-        index = data["index"]
-        line = line.strip()
-
-        sums = map(
-            create_counter(line, index),
-            slopes,
-            data["sums"]
-        )
-
-        return {
-            "sums": list(sums),
-            "index": index + 1
-        }
+    def sum_octos(sums, item):
+        return list(map(
+            create_counter(*item), 
+            slopes, 
+            sums
+        ))
 
     return sum_octos
 
@@ -39,10 +33,9 @@ with open('./inputs/input-0C.txt') as f:
         (1, 2)
     ]
 
-    data = reduce(create_sum(slopes), f.readlines(), {
-        "sums": [0, 0, 0, 0, 0],
-        "index": 0
-    })
-
-    print(data)
-    print(reduce(lambda multi, num: multi * num, data["sums"]))
+    data = reduce(
+        create_sum(slopes), 
+        enumerate(strip_lines(f)), 
+        [0]*len(slopes) # <-- Initial value: a list od 0 the same size as slopes 
+    ) 
+    print(prod(data))
